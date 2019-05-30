5e90efa0dcdba57dca669b6a8452e6247494c936
Kristo Kuusküll <onukristo@gmail.com>
2019-05-30 12:34:39 +0300

    Published artifacts to Sonatype public repos.

--- tw-tasks-executor/src/test/groovy/com/transferwise/tasks/testappa/config/CuratorConfiguration.java
+++ tw-tasks-executor/src/test/groovy/com/transferwise/tasks/testappa/config/CuratorConfiguration.java
@@ -4,7 +4,7 @@ import com.transferwise.tasks.TasksProperties;
 import com.transferwise.tasks.helpers.executors.ExecutorThreadFactory;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.RetryForever;
+import org.apache.curator.retry.RetryNTimes;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -16,7 +16,7 @@ public class CuratorConfiguration {
     public CuratorFramework curatorFramework(@Autowired(required = false) TestContainersManager testContainersManager,
                                              TasksProperties tasksProperties) {
         CuratorFramework curatorFramework = CuratorFrameworkFactory.builder().connectionTimeoutMs(5000).sessionTimeoutMs(300000).canBeReadOnly(false)
-            .connectString(tasksProperties.getZookeeperConnectString()).retryPolicy(new RetryForever(1000))
+            .connectString(tasksProperties.getZookeeperConnectString()).retryPolicy(new RetryNTimes(5, 1000))
             .threadFactory(new ExecutorThreadFactory("curator")).build();
 
         curatorFramework.start();
