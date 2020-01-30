7430b64c11d90644150914bdc0cbc9c173a0beeb
Yevhenii Voievodin <yevhenii.voievodin@transferwise.com>
2020-01-30 16:25:17 +0200

    Create resilient kafka message handler

---
+++ tw-tasks-executor/src/main/java/com/transferwise/tasks/helpers/kafka/messagetotask/CreateTaskForCorruptedMessageRecoveryStrategy.java
@@ -0,0 +1,47 @@
+package com.transferwise.tasks.helpers.kafka.messagetotask;
+
+import com.transferwise.tasks.ITasksService;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+/**
+ * Recovery policy that creates a task of the given type (default {@value DEFAULT_CORRUPTED_MESSAGE_TASK_TYPE})
+ * wrapping the original "corrupted" content in {@link CorruptedKafkaMessage}.
+ * The task of created type requires manual intervention, which basically means that
+ * a dedicated handler resolving corrupted messages needs to be implemented.
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class CreateTaskForCorruptedMessageRecoveryStrategy implements CorruptedMessageRecoveryStrategy {
+
+    public static final String DEFAULT_CORRUPTED_MESSAGE_TASK_TYPE = "CORRUPTED_KAFKA_MESSAGE";
+
+    private final ITasksService tasksService;
+    private final String type;
+
+    public CreateTaskForCorruptedMessageRecoveryStrategy(ITasksService tasksService) {
+        this(tasksService, DEFAULT_CORRUPTED_MESSAGE_TASK_TYPE);
+    }
+
+    @Override
+    public void recover(Class<?> messageType, ConsumerRecord<String, String> record, Exception exception) {
+        log.error("Failed to deserialize message from topic {}, creating a {} task for it", record.topic(), type, exception);
+        tasksService.addTask(
+            new ITasksService.AddTaskRequest()
+                .setType(type)
+                .setData(new CorruptedKafkaMessage(record.topic(), record.value()))
+        );
+    }
+
+    @Data
+    @AllArgsConstructor
+    @NoArgsConstructor
+    public static class CorruptedKafkaMessage {
+        private String topic;
+        private String corruptedData;
+    }
+}
---
+++ tw-tasks-executor/src/main/java/com/transferwise/tasks/helpers/kafka/messagetotask/KafkaMessageHandlerFactory.java
@@ -0,0 +1,94 @@
+package com.transferwise.tasks.helpers.kafka.messagetotask;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.transferwise.tasks.ITasksService;
+import com.transferwise.tasks.utils.TriConsumer;
+import lombok.RequiredArgsConstructor;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.springframework.stereotype.Component;
+
+/** Factory for ready to use {@link IKafkaMessageHandler kafka message handlers}. */
+@Component
+@RequiredArgsConstructor
+public class KafkaMessageHandlerFactory {
+
+    private final ITasksService tasksService;
+    private final ObjectMapper objectMapper;
+
+    /**
+     * Creates default handler, the behaviour of the handler is the same to {@link KafkaMessageToTaskConverter}.
+     * When the handler fails to deserialize a message it will infinitely attempt to deserialize the same message
+     * over and over again, the processing of the other messages in the topic will be stuck.
+     */
+    public <T> IKafkaMessageHandler<String> createDefaultHandler(
+        Class<T> dataObjClass,
+        TriConsumer<T, ConsumerRecord<String, String>, ITasksService.AddTaskRequest> consumer,
+        IKafkaMessageHandler.Topic... topics
+    ) {
+        return createResilientHandler(
+            new InfiniteRetryCorruptedMessageRecoveryStrategy(),
+            dataObjClass,
+            consumer,
+            topics
+        );
+    }
+
+    /**
+     * Creates a resilient handler, every corrupted message will be wrapped and
+     * processed separately as described in {@link CorruptedMessageRecoveryStrategy}.
+     *
+     * @see ResilientKafkaMessageHandler
+     */
+    public <T> IKafkaMessageHandler<String> createDefaultResilientHandler(
+        Class<T> dataObjClass,
+        TriConsumer<T, ConsumerRecord<String, String>, ITasksService.AddTaskRequest> consumer,
+        IKafkaMessageHandler.Topic... topics
+    ) {
+        return createResilientHandler(
+            new CreateTaskForCorruptedMessageRecoveryStrategy(tasksService),
+            dataObjClass,
+            consumer,
+            topics
+        );
+    }
+
+    /**
+     * Creates a resilient handler, every corrupted message will be wrapped and
+     * processed separately as described in {@link CorruptedMessageRecoveryStrategy}.
+     *
+     * @see ResilientKafkaMessageHandler
+     */
+    public <T> IKafkaMessageHandler<String> createDefaultResilientHandler(
+        Class<T> dataObjClass,
+        TriConsumer<T, ConsumerRecord<String, String>, ITasksService.AddTaskRequest> consumer,
+        String topicAddress
+    ) {
+        return createDefaultResilientHandler(
+            dataObjClass,
+            consumer,
+            new IKafkaMessageHandler.Topic().setAddress(topicAddress)
+        );
+    }
+
+    /**
+     * Creates a resilient handler, the decision on how to deal with
+     * corrupted messages is delegated to the given {@code recoveryStrategy}.
+     *
+     * @see ResilientKafkaMessageHandler
+     */
+    public <T> IKafkaMessageHandler<String> createResilientHandler(
+        CorruptedMessageRecoveryStrategy recoveryStrategy,
+        Class<T> dataObjClass,
+        TriConsumer<T, ConsumerRecord<String, String>, ITasksService.AddTaskRequest> consumer,
+        IKafkaMessageHandler.Topic... topics
+    ) {
+        return new ResilientKafkaMessageHandler<>(
+            objectMapper,
+            tasksService,
+            recoveryStrategy,
+            dataObjClass,
+            consumer,
+            topics
+        );
+    }
+}
