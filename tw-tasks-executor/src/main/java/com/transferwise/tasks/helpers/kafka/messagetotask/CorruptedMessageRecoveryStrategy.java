4bce1036cd5fe34e2c09b1db222d870104db6943
Yevhenii Voievodin <yevhenii.voievodin@transferwise.com>
2020-01-30 16:25:17 +0200

    Create resilient kafka message handler

---
+++ tw-tasks-executor/src/main/java/com/transferwise/tasks/helpers/kafka/messagetotask/CorruptedMessageRecoveryStrategy.java
@@ -0,0 +1,20 @@
+package com.transferwise.tasks.helpers.kafka.messagetotask;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+/**
+ * The strategy used to recover corrupted message (the one that cannot be deserialized)
+ * while processing it with {@link ResilientKafkaMessageHandler}.
+ */
+public interface CorruptedMessageRecoveryStrategy {
+
+    /**
+     * Recovers from infinite processing of the same kafka message.
+     *
+     * @param messageType the type of the message deserialization was failed for
+     * @param record      the record that was failed to be deserialized
+     * @param exception   the exception appeared as deserialization failure
+     * @throws Exception in case recovery is not sufficient and the same record needs to be reprocessed again
+     */
+    void recover(Class<?> messageType, ConsumerRecord<String, String> record, Exception exception) throws Exception;
+}
---
+++ tw-tasks-executor/src/main/java/com/transferwise/tasks/helpers/kafka/messagetotask/InfiniteRetryCorruptedMessageRecoveryStrategy.java
@@ -0,0 +1,11 @@
+package com.transferwise.tasks.helpers.kafka.messagetotask;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+
+/** Doesn't do any recovery, the message deserialization will be retried again. */
+public class InfiniteRetryCorruptedMessageRecoveryStrategy implements CorruptedMessageRecoveryStrategy {
+    @Override
+    public void recover(Class<?> messageType, ConsumerRecord<String, String> record, Exception exception) throws Exception {
+        throw exception;
+    }
+}
