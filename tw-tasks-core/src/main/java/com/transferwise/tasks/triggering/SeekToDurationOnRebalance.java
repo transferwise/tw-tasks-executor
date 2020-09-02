2e471b2852e4f8ef500a24f8911ba7bda9e0c568
Yevhenii Voievodin <yevhenii.voievodin@transferwise.com>
2020-09-02 21:18:01 +0300

    Extract SeekToDurationOnRebalance listener, add tests

---
+++ tw-tasks-core/src/main/java/com/transferwise/tasks/triggering/SeekToDurationOnRebalance.java
@@ -0,0 +1,70 @@
+package com.transferwise.tasks.triggering;
+
+import com.transferwise.common.context.TwContextClockHolder;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndTimestamp;
+import org.apache.kafka.common.TopicPartition;
+
+/**
+ * For any partition that doesn't have offset committed for a given consumer
+ * - seeks to the earliest offset that is greater than {@code now() - autoResetOffsetToDuration}.
+ * Seeks to the beginning in case the timestamp of the very first record in partition
+ * has value greater than {@code now() - autoResetOffsetToDuration}.
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class SeekToDurationOnRebalance implements ConsumerRebalanceListener {
+
+  private final KafkaConsumer<?, ?> consumer;
+  private final Duration autoResetOffsetToDuration;
+
+  @Override
+  public void onPartitionsRevoked(Collection<TopicPartition> partitions) {
+  }
+
+  @Override
+  public void onPartitionsAssigned(Collection<TopicPartition> partitions) {
+    Map<TopicPartition, Long> timestampsToSearch = new HashMap<>();
+    long timestampToSearchMs = ZonedDateTime.now(TwContextClockHolder.getClock()).plus(autoResetOffsetToDuration).toInstant().toEpochMilli();
+
+    for (TopicPartition partition : partitions) {
+      try {
+        if (consumer.committed(partition) == null) {
+          timestampsToSearch.put(partition, timestampToSearchMs);
+        }
+      } catch (Throwable t) {
+        timestampsToSearch.put(partition, timestampToSearchMs);
+      }
+    }
+
+    if (!timestampsToSearch.isEmpty()) {
+      List<TopicPartition> seekToBeginningPartitions = new ArrayList<>();
+
+      Map<TopicPartition, OffsetAndTimestamp> offsets = consumer.offsetsForTimes(timestampsToSearch);
+      offsets.forEach((k, v) -> {
+        if (v != null) {
+          log.warn("No offset was committed for '" + k + "', seeking to offset " + v.offset() + ", @" + Instant.ofEpochMilli(v.timestamp()));
+          consumer.seek(k, v.offset());
+        } else {
+          log.warn("No offset was committed for '" + k + "', seeking to beginning");
+          seekToBeginningPartitions.add(k);
+        }
+      });
+
+      if (!seekToBeginningPartitions.isEmpty()) {
+        consumer.seekToBeginning(seekToBeginningPartitions);
+      }
+    }
+  }
+}
