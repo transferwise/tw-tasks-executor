0fbe63c5551c7b7e443c323d793eb9edf20bc36d
bbofosu <bbofosu@gmail.com>
2020-10-31 16:08:59 +0200

    missed commit

---
+++ integration-tests/src/test/java/com/transferwise/tasks/testapp/dao/JdbcDaoIntTest.java
@@ -0,0 +1,23 @@
+package com.transferwise.tasks.testapp.dao;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import java.util.List;
+
+public abstract class JdbcDaoIntTest extends TaskDaoIntTest {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Override
+  List<String> findDataByType(String taskType) {
+    return jdbcTemplate.queryForList("select data from tw_task where type=? order by id", String.class, taskType);
+  }
+
+  @Override
+  int getUniqueTaskKeysCount() {
+    Integer cnt = jdbcTemplate.queryForObject("select count(*) from unique_tw_task_key", Integer.class);
+    // Just keep the spotbugs happy.
+    return cnt == null ? 0 : cnt;
+  }
+}
