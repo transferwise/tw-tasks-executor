588cadb1c346d077ded995035f99517d48307ccc
Louay Mohamed <louay.mohamed@transferwise.com>
2020-11-11 19:42:22 +0800

    fix checkstyle

--- tw-tasks-core/src/main/java/com/transferwise/tasks/helpers/kafka/ITopicPartitionsManager.java
+++ tw-tasks-core/src/main/java/com/transferwise/tasks/helpers/kafka/ITopicPartitionsManager.java
@@ -6,5 +6,6 @@ import org.springframework.boot.autoconfigure.kafka.KafkaProperties;
 public interface ITopicPartitionsManager {
 
   AdminClient createKafkaAdminClient(KafkaProperties kafkaProperties);
+
   void setPartitionsCount(String topic, int partitionsCount);
 }
