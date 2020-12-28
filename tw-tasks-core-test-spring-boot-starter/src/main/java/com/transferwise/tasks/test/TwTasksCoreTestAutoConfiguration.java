75846f06cd53bdcdd21bb98d80514a12c4217c85
Kristo Kuusküll <onukristo@gmail.com>
2020-12-28 14:24:50 +0200

    Test coverage (#87)
    * Added test coverage for old tasks having old data field set.

--- tw-tasks-core-test-spring-boot-starter/src/main/java/com/transferwise/tasks/test/TwTasksCoreTestAutoConfiguration.java
+++ tw-tasks-core-test-spring-boot-starter/src/main/java/com/transferwise/tasks/test/TwTasksCoreTestAutoConfiguration.java
@@ -10,7 +10,6 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class TwTasksCoreTestAutoConfiguration {
@@ -32,7 +31,6 @@ public class TwTasksCoreTestAutoConfiguration {
   }
 
   @Bean
-  @Primary()
   public TestTasksService twTasksTestTasksService() {
     return new TestTasksService();
   }
